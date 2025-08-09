2812a7a87a0d1b2efbed553fee1c7f9ce3387d4e
Sahal Bin Amnin <sahalbinamin.37@gmail.com>
2025-08-09 14:56:17 +0500

    React routing implemented

---
+++ src/landing_Page/home/HomePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Pricing from "./Pricing";
+import Education from "./Education";
+import Stats from "./Stats";
+import OpenAccount from "../OpenAccount";
+import Nav from "../Nav";
+import Footer from "../Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+    </>
+  );
+}
+
+export default HomePage;
